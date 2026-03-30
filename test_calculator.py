fd04103593d6e5d546d212cb5543c684e870498f
Ziaurrehman <2025sl93043@wilp.bits-pilani.ac.in>
2026-03-30 11:18:21 +0530

    Added test file

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import calculator
+# Test add
+assert calculator.add(2, 3) == 5
+# Test sub
+assert calculator.substract(5, 2) == 3
+# Test mul
+assert calculator.multiply(2, 3) == 6
+# Test div
+assert calculator.divide(6, 2) == 3
+print("All tests passed successfully!")
